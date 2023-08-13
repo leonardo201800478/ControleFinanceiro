a75d49513385bdeb2fddd5c3a589e3f449025b27
leost <leost@LEOSTRIFE>
2023-08-13 16:42:36 -0300

    Update inserindo a página About Adicionar arquivos de projeto.

---
+++ Apoio/Controllers/ApoioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Apoio.Controllers
+{
+    public class ApoioController : Controller
+    {
+        public IActionResult Empresa()
+        {
+            ViewData["Title"] = "Controle de Empresas";
+            return View();
+        }
+    }
+}
---
+++ Apoio/Views/Apoio/Empresa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Apoio.Views.Apoio
+{
+    public class EmpresaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
